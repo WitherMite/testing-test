e3d40e69d81116bcdcb017697af74c29cc445966
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-08-12 11:48:41 -0500

    Capitalize only accepts strings

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,6 @@
 export default function capitalize(str) {
+  if (!(typeof str === "string"))
+    throw new Error("Argument is not a valid string");
   const strArr = str.split("");
   strArr[0] = strArr[0].toUpperCase();
   return strArr.join("");
