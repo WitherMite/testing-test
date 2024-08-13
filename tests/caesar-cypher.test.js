9208cdac8690c6911443571c16f952335aa9f20a
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-08-13 15:49:04 -0500

    Test caesar cypher shifts letters in strings

---
+++ tests/caesar-cypher.test.js
@@ -0,0 +1,33 @@
+import caesarCypher from "../src/caesar-cypher.js";
+
+describe("Shifts letters", () => {
+  test.each([
+    ["abc", 3, "def"],
+    ["hello", 7, "olssv"],
+    ["xyz", -2, "vwx"],
+    ["bob", 6, "huh"],
+    ["nfn", -17, "wow"],
+  ])("Shifts %s %s letters to %s", (string, shift, cypher) => {
+    expect(caesarCypher(string, shift)).toBe(cypher);
+  });
+});
+
+describe("Only accepts strings", () => {
+  test.each([
+    [undefined],
+    [null],
+    [NaN],
+    [true],
+    [1],
+    [0],
+    [Infinity],
+    [{ a: 1, b: 2 }],
+    [
+      () => {
+        "hello-world";
+      },
+    ],
+  ])("Errors when passed %p", (value) => {
+    expect(() => caesarCypher(value)).toThrow();
+  });
+});
