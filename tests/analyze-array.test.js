9f925f9933e1b92df4e1a0ca8e951c7160fdf419
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-08-14 10:51:12 -0500

    Test returned object has correct properties

--- tests/analyze-array.test.js
+++ tests/analyze-array.test.js
@@ -6,6 +6,23 @@ test("Outputs an object", () => {
   expect(isObj).toBe(true);
 });
 
+describe("Object has specified properties", () => {
+  const resultOne = analyzeArray([69]);
+  const resultTwo = analyzeArray([1, 2, 3, 4, 5]);
+  const resultThree = analyzeArray([-4, 56, 12, 100, 0, 4]);
+
+  test.each([
+    ["average", 69, 3, 28],
+    ["min", 69, 1, -4],
+    ["max", 69, 5, 100],
+    ["length", 1, 5, 6],
+  ])("Object has correct %s property", (property, val1, val2, val3) => {
+    expect(resultOne[property]).toBe(val1);
+    expect(resultTwo[property]).toBe(val2);
+    expect(resultThree[property]).toBe(val3);
+  });
+});
+
 describe("Only accepts arrays", () => {
   test.each([
     [undefined],
